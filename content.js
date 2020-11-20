fb8fe48497771d63e409657cfc016f2597a35e60
Chanbin Lee <chanbin.lee123@gmail.com>
2020-11-20 15:18:13 +0900

    Added dog pictures too

--- content.js
+++ content.js
@@ -18,7 +18,6 @@ function onWindowLoaded() {
 
     function replace() {
         let images = document.getElementsByTagName('img');
-        console.log(images);
         for (image of images) {
             if (image.dataset.replaced == undefined) {
                 changeImg(image);
@@ -28,17 +27,22 @@ function onWindowLoaded() {
 
     function changeImg(img) {
         img.dataset.replaced = true;
-        width = img.width;
-        height = img.height;
+        width = img.width - (img.width%10);
+        height = img.height - (img.height%10);
         img.src = makeURL(width, height);
         // let ran = Math.floor(Math.random() * 10);
         // let file = 'cat/' + ran + '.jpg';
         // let url = chrome.extension.getURL(file);
         // img.src = url;
+        return;
     }
 
     function makeURL(x, y) {
-        return 'http://placekitten.com/'+x+'/'+y;
+        let ran = Math.floor(Math.random() * 2);
+        if(ran===0)
+            return 'http://placekitten.com/'+x+'/'+y;
+        if(ran===1)
+            return 'https://placedog.net/'+x+'/'+y+'?random';
     }
 
 
