dd0504b3dcd85874eba31384a731ae560f569334
Chanbin Lee <chanbin.lee123@gmail.com>
2020-11-20 13:57:31 +0900

    updates on new loads

--- content.js
+++ content.js
@@ -1,24 +1,39 @@
-//Prevents code from running before page
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', replace);
-} else {
-    replace();
-}
+window.addEventListener('load', onWindowLoaded, false);
 
-function replace() {
-    console.log("wopp");
-    let imgs = document.getElementsByTagName('img');
-    console.log(imgs);
-    console.log(imgs.length);
-    for (image of imgs) {
-        let ran = Math.floor(Math.random() * 10);
-        console.log(ran);
+function onWindowLoaded() {
+    const target = document.body;
+    const config = { attributes: false, childList: true, subtree: true };
+    const observer = new MutationObserver(checkSubtree);
+    observer.observe(target, config);
 
-        let file = 'cat/' + ran + '.jpg';
-        let url = chrome.extension.getURL(file);
-        image.src = url;
+    function checkSubtree(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                replace();
+            }
+        }
     }
 }
+
+
+    function replace() {
+        let images = document.getElementsByTagName('img');
+        console.log(images);
+        for (image of images) {
+            if (image.dataset.replaced == undefined) {
+                changeImg(image);
+            }
+        }
+    }
+
+    function changeImg(img) {
+        img.dataset.replaced = true;
+        let ran = Math.floor(Math.random() * 10);
+        let file = 'cat/' + ran + '.jpg';
+        let url = chrome.extension.getURL(file);
+        img.src = url;
+    }
+
 
 
 
